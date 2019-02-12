fcd00b734d67e1940397627ed59c2c246c3b814d
Macbook <macbook@nikolay-macbook.local>
2019-02-12 13:49:41 +0200

    Commit and push test

--- DZL3 Romanov/ViewController.swift
+++ DZL3 Romanov/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Commit and push test
         maxMumber(numberOne: 2, numberTow: 2)
         squareAndCubeOfNumber(number: 2)
         numberOrder(givenNumber: 5)
